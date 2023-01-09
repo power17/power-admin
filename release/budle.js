45c2851aa8e1d6da9a2331c7cbd7d8d82b157b05
power <1410837981@qq.com>
2023-01-09 23:24:00 +0800

    V0.0.1 初次提交

--- release/budle.js
+++ release/budle.js
@@ -16,7 +16,7 @@
   \**********************/
 /***/ (() => {
 
-eval("\n\nalert('hello world');\n\n//# sourceURL=webpack://fast-cache/./src/index.js?");
+eval("\n\nvar _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();\n\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nvar FastCache = function () {\n  function FastCache() {\n    _classCallCheck(this, FastCache);\n\n    this.list = {};\n  }\n\n  _createClass(FastCache, [{\n    key: \"set\",\n    value: function set(key, value) {\n      this.list[key] = value;\n    }\n  }, {\n    key: \"get\",\n    value: function get(key) {\n      return this.list[key];\n    }\n  }]);\n\n  return FastCache;\n}();\n\nconsole.log(FastCache);\nwindow.FastCache = FastCache;\n\n//# sourceURL=webpack://fast-cache/./src/index.js?");
 
 /***/ })
 
