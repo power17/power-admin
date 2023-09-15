52318dbd3e625cdf6f23835e9f73c1632d9d98ab
power <1410837981@qq.com>
2023-09-15 18:54:11 +0800

    build(asset): 📦️ 增加vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,55 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-
+import { defineConfig, ConfigEnv, UserConfig } from 'vite';
+import path from 'path';
+import { createProxy } from './build/proxy';
+import { createVitePlugins } from './build/plugins';
+import { wrapperEnv } from './build/getEnv';
+import { loadEnv } from 'vite';
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const pwd = process.cwd();
+  const env = loadEnv(mode, pwd);
+  const viteEnv = wrapperEnv(env);
+
+  return {
+    base: viteEnv.VITE_PUBLIC_PATH,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+      extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.vue', '.mjs'],
+    },
+    server: {
+      host: '0.0.0.0',
+      port: viteEnv.VITE_PORT,
+      open: viteEnv.VITE_OPEN,
+      cors: true,
+      // Load proxy configuration from .env.development
+      proxy: createProxy(viteEnv.VITE_PROXY),
+    },
+    plugins: createVitePlugins(viteEnv),
+    build: {
+      outDir: 'dist',
+      minify: 'esbuild',
+      // esbuild 打包更快，但是不能去除 console.log，terser打包慢，但能去除 console.log
+      // minify: "terser",
+      // terserOptions: {
+      // 	compress: {
+      // 		drop_console: viteEnv.VITE_DROP_CONSOLE,
+      // 		drop_debugger: true
+      // 	}
+      // },
+      // 禁用 gzip 压缩大小报告，可略微减少打包时间
+      reportCompressedSize: false,
+      // 规定触发警告的 chunk 大小
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        output: {
+          // Static resource classification and packaging
+          chunkFileNames: 'assets/js/[name]-[hash].js',
+          entryFileNames: 'assets/js/[name]-[hash].js',
+          assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+        },
+      },
+    },
+  };
 });
